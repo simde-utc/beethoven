337a1d6d323dc8c404103f6fe79c3f875a513e08
OBLED Aymeric <obled.aymeric@gmail.com>
2018-09-04 14:12:11 +0200

    add header and body file, change colours

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './header';
+import Body from './body';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Beethoven by Payutc</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header></Header>
+        <Body></Body>
       </div>
     );
   }
---
+++ src/body.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Body extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <p className="App-intro">
+          To be continued.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Body;
