322027d4cd11c0b156960f5711f0b125b2d5bd09
OBLED Aymeric <obled.aymeric@gmail.com>
2018-09-28 15:55:59 +0200

    add cas connection + todo check rights

--- src/App.js
+++ src/App.js
@@ -5,15 +5,39 @@ import Header from './header';
 import Body from './body';
 import MenuBody from './Menu/menu';
 
+
 class App extends Component {
-  render() {
+
+    constructor(props){
+        super(props)
+        this.state = {
+        cas_ticket: null
+        }
+    }
+    check_cas_ticket(){
+        if (this.state.cas_ticket==null){
+            window.location.href = 'https://cas.utc.fr/cas/login?service=http://beethoven.picasso-utc.fr?task=login';
+            let ticket = getTicket(window.location.href);
+            this.setState({cas_ticket: ticket})
+        }
+    }
+    getTicket(url){
+      let ticketRegex = /(\?|&)ticket=([^&=]+)/;
+      if(ticketRegex.test(url)){
+        let match = ticketRegex.exec(url);
+        return match[2];
+       }
+    }
+
+    render() {
+    this.check_cas_ticket()
     return (
       <div className="App">
         <Header></Header>
         <MenuBody></MenuBody>
       </div>
     );
-  }
+    }
 }
 
 export default App;
