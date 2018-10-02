c6a4273d53077b4f6a11a3127ceb5a6e85024c33
hugofloter <hugo.paigneau@etu.utc.fr>
2018-10-02 16:00:55 +0200

    COnnexion CAS

--- src/App.js
+++ src/App.js
@@ -5,57 +5,141 @@ import Header from './header';
 import Body from './body';
 import MenuBody from './Menu/menu';
 
-setTicket(ticket){
+
+
+function setTicket(ticket){
     this.setState({ticket})
 }
 
-class CasConnection {
-    getTicket(url){
-      let ticketRegex = /(\?|&)ticket=([^&=]+)/;
-      if(ticketRegex.test(url)){
-        let match = ticketRegex.exec(url);
-        return match[2];
-       }
-    }
 
-    check_cas_ticket(){
-        console.log(this.state.cas_ticket)
-        if (this.state.cas_ticket==null){
-            window.location.href = 'https://cas.utc.fr/cas/login?service=http://beethoven.picasso-utc.fr?task=login';
-            let ticket = this.getTicket(window.location.href);
-            this.setState({cas_ticket: ticket})
-        }
+function getTicket(url){
+  let ticketRegex = /(\?|&)ticket=([^&=]+)/;
+  if(ticketRegex.test(url)){
+    let match = ticketRegex.exec(url);
+    return match[2];
+   }
+}
+
+function getCasUrl(tick){
+  let casurl = 'https://cas.utc.fr/cas/'
+  let serviceurl = 'http://beethoven.picasso-utc.fr/'
+  fetch('https://api.nemopay.net/services/ROSETTINGS/getCasUrl?system_id=payutc&app_key=31f5809f43b3161dd8aefb7a79a5fc55',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Nemopay-Version': '2018-07-03',
+        },
+      })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          fetch("https://api.nemopay.net/services/MYACCOUNT/loginCas2?system_id=payutc&app_key=31f5809f43b3161dd8aefb7a79a5fc55", {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Nemopay-Version': '2018-07-03',
+          },
+          body: JSON.stringify({
+              ticket: tick,
+              service: serviceurl
+          }),
+          })
+          .then(res => res.json())
+          .then(
+              (result) => {
+              console.log('log VIA CAS effect')
+              console.log(result)
+          },
+          (error) => {
+             ('La ça bug un peu')
+          })
+
+
+
+
+
+
+
+        },
+        (error) => {
+          ('Problem1')
+      });
+
+}
+
+
+function connectToCas(t){
+  getCasUrl(t);
+}
+
+
+function check_cas_ticket(){
+        let ticket = getTicket(window.location.href);
+        return ticket;
+}
+
+class CasTicket extends Component {
+  constructor(props){
+      super(props)
+      this.state = {
+      loaded: false
+      }
+      this.redir = this.redir.bind(this);
+  }
+  redir() {
+    if(this.state.loaded==false){
+      let tickt = check_cas_ticket();
+      setTicket(tickt)
+      this.setState({loaded: true})
+    }else{
+      console.log('Cst okokokokookok')
     }
+  }
+  render() {
+    this.redir()
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+class CasConnection extends Component {
+  constructor(props){
+      super(props)
+      this.state = {
+        ticket: null
+      }
+      setTicket = setTicket.bind(this);
+  }
+  componentDidMount(){
+    console.log(this.state.ticket);
+  }
+  render() {
+    if(this.state.ticket!==null){
+      connectToCas(this.state.ticket);
+    }
+    console.log(this.state.ticket);
+    return (
+      <div>
+      </div>
+    );
+  }
 }
 
 class App extends Component {
-
-    constructor(props){
-        super(props)
-        this.state = {
-        cas_ticket: null
-        }
-    }
-
-    componentDidMount() {
-        this.setState({cas_ticket: null})
-    }
-
-    componentDidUpdate(){
-    console.log("in did update")
-        this.check_cas_ticket()
-    }
-
-
-
-
     render() {
-    return (
-      <div className="App">
-        <Header></Header>
-        <MenuBody></MenuBody>
-      </div>
-    );
+      getCasUrl();
+      return (
+        <div className="App">
+          <Header></Header>
+          <MenuBody></MenuBody>
+          <CasConnection></CasConnection>
+          <CasTicket></CasTicket>
+        </div>
+      );
     }
 }
 
