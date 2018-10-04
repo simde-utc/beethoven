eea52ac3258b7eee861f631e8eec739e30f7d563
hugofloter <hugo.paigneau@etu.utc.fr>
2018-10-04 17:03:58 +0200

    COnnection au CAS opérationnelle

--- src/App.js
+++ src/App.js
@@ -4,149 +4,16 @@ import './App.css';
 import Header from './header';
 import Body from './body';
 import MenuBody from './Menu/menu';
-
-
-
-function setTicket(ticket){
-    this.setState({ticket})
-}
-
-
-function getTicket(url){
-  let ticketRegex = /(\?|&)ticket=([^&=]+)/;
-  if(ticketRegex.test(url)){
-    let match = ticketRegex.exec(url);
-    return match[2];
-   }
-}
-
-function getCasUrl(tick){
-  let casurl = 'https://cas.utc.fr/cas/'
-  let serviceurl = 'http%3A%2F%2Flocalhost%3A5000'
-  fetch('https://api.nemopay.net/services/ROSETTINGS/getCasUrl?system_id=payutc&app_key=31f5809f43b3161dd8aefb7a79a5fc55',{
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Nemopay-Version': '2018-07-03',
-        },
-      })
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log(result)
-          let response = fetch(casurl+'v1/tickets?service='+serviceurl+'&username=LOGIN HERE&password=PASSWORD HERE',{
-                method: 'POST',
-                headers: {
-                  'Content-type': 'application/x-www-form-urlencoded',
-              		'Accept': 'text/plain',
-                },
-              });
-          console.log(response.status)
-
-          fetch("https://api.nemopay.net/services/MYACCOUNT/loginCas2?system_id=payutc&app_key=31f5809f43b3161dd8aefb7a79a5fc55", {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-              'Nemopay-Version': '2018-07-03',
-          },
-          body: JSON.stringify({
-              ticket: tick,
-              service: serviceurl
-          }),
-          })
-          .then(res => res.json())
-          .then(
-              (result) => {
-              console.log('log VIA CAS effect')
-              console.log(result)
-          },
-          (error) => {
-             ('La ça bug un peu')
-          })
-
-
-
-
-
-
-
-        },
-        (error) => {
-          ('Problem1')
-      });
-
-}
-
-
-function connectToCas(t){
-  getCasUrl(t);
-}
-
-
-function check_cas_ticket(){
-        let ticket = getTicket(window.location.href);
-        return ticket;
-}
-
-class CasTicket extends Component {
-  constructor(props){
-      super(props)
-      this.state = {
-      loaded: false
-      }
-      this.redir = this.redir.bind(this);
-  }
-  redir() {
-    if(this.state.loaded==false){
-      let tickt = check_cas_ticket();
-      setTicket(tickt)
-      this.setState({loaded: true})
-    }else{
-      console.log('Cst okokokokookok')
-    }
-  }
-  render() {
-    this.redir()
-    return (
-      <div>
-
-      </div>
-    );
-  }
-}
-
-class CasConnection extends Component {
-  constructor(props){
-      super(props)
-      this.state = {
-        ticket: null
-      }
-      setTicket = setTicket.bind(this);
-  }
-  componentDidMount(){
-    console.log(this.state.ticket);
-  }
-  render() {
-    if(this.state.ticket!==null){
-      connectToCas(this.state.ticket);
-    }
-    console.log(this.state.ticket);
-    return (
-      <div>
-      </div>
-    );
-  }
-}
+import CasConnection from './config';
 
 class App extends Component {
     render() {
-      getCasUrl();
       return (
         <div className="App">
           <Header></Header>
           <MenuBody></MenuBody>
+          
           <CasConnection></CasConnection>
-          <CasTicket></CasTicket>
         </div>
       );
     }
---
+++ src/config.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import './App.css';
+
+{/*Fichier de connexion au CAS*/}
+
+{/*Fonction pour transmettre le ticket à la classe de connection au cas*/}
+function setTicket(ticket){
+    this.setState({ticket})
+}
+
+{/*Récupérer le ticket de l'url*/}
+function getTicket(url){
+  let ticketRegex = /(\?|&)ticket=([^&=]+)/;
+  if(ticketRegex.test(url)){
+    let match = ticketRegex.exec(url);
+    return match[2];
+   }
+}
+
+{/*Retour du ticket*/}
+function check_cas_ticket(){
+        let ticket = getTicket(window.location.href);
+        return ticket;
+}
+
+{/*Classe qui choppe le ticket et le redirige vers la classe de connection*/}
+class CasTicket extends Component {
+  constructor(props){
+      super(props)
+      this.state = {
+      loaded: false
+      }
+      this.redir = this.redir.bind(this);
+  }
+  redir() {
+    if(this.state.loaded==false){
+      let tickt = check_cas_ticket();
+      setTicket(tickt)
+      this.setState({loaded: true})
+    }else{
+      console.log('Cst okokokokookok')
+    }
+  }
+  render() {
+    this.redir()
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+
+{/*Classe permettant la connection au CAS et récupération du sessionid dans le state*/}
+class GetSessionId extends Component {
+  constructor(props){
+      super(props)
+      this.state = {
+        ticket: null,
+        sessionid: null
+      }
+      setTicket = setTicket.bind(this);
+  }
+  getCasUrl(tick){
+    if(tick){
+      let serviceurl = 'http://localhost:5000'
+      fetch("https://api.nemopay.net/services/MYACCOUNT/loginCas2?system_id=payutc&app_key=31f5809f43b3161dd8aefb7a79a5fc55", {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Nemopay-Version': '2018-07-03',
+      },
+      body:  '{"ticket":"'+tick+'","service":"'+serviceurl+'"}',
+      })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log('log VIA CAS effect')
+          this.setState({sessionid: result.sessionid})
+      },
+        (error) => {
+         ('La ça bug un peu')
+      })
+    }
+  }
+  render() {
+    if(this.state.ticket!==null && this.state.sessionid==null){
+      this.getCasUrl(this.state.ticket);
+    }
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+{/*Classe finale combinant les 2 précédentes*/}
+class CasConnection extends Component {
+  render() {
+    return (
+      <div>
+        <GetSessionId></GetSessionId>
+        <CasTicket></CasTicket>
+      </div>
+    );
+  }
+}
+
+export default CasConnection;
