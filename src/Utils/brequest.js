d35d18b326085c00842fc469d2dff06fc2ee253f
quentin <quentinrichard97@gmail.com>
2018-10-19 21:09:07 +0200

    fixed no-cache

--- src/Utils/brequest.js
+++ src/Utils/brequest.js
@@ -7,7 +7,7 @@ const brequest = (server, method, service = null , request, data = null, session
     case 'apiRequest':
       url = sessionid === null ?
       API_URL + service + '/' + request + '?system_id=payutc&app_key='+WEEZEVENT_APP_KEY :
-      API_URL + service + '/' + request + '?system_id=payutc&app_key='+WEEZEVENT_APP_KEY + '&sessionid='+sessionid 
+      API_URL + service + '/' + request + '?system_id=payutc&app_key='+WEEZEVENT_APP_KEY + '&sessionid='+sessionid
       return fetch(
         url,
         {
@@ -16,14 +16,15 @@ const brequest = (server, method, service = null , request, data = null, session
             'Content-Type': 'application/json',
             'Nemopay-Version': '2018-07-03',
           },
-          body : JSON.stringify(data)
+          body : JSON.stringify(data),
+          cache : 'no-store'
         }
       )
 
     case 'picsousRequest':
     url  = data!== null ?
-    PICSOUS_URL +  request + '/' + data +'?random='+Math.random() :
-    PICSOUS_URL +  request +'?random='+Math.random()
+    PICSOUS_URL +  request + '/' + data  :
+    PICSOUS_URL +  request 
 
     return fetch(
       url,
@@ -32,6 +33,7 @@ const brequest = (server, method, service = null , request, data = null, session
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         },
+        cache : 'no-store'
       }
     )
 
