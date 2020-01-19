405ad717ae52d01fe93f92622e371266d304305c
quentin <quentinrichard97@gmail.com>
2020-01-19 19:08:10 +0100

    config file

---
+++ src/config_example.js
@@ -0,0 +1,9 @@
+export const WEEZEVENT_APP_KEY = ''
+export const WEEZEVENT_SYSTEM_ID = ''
+export const WEEZEVENT_URL = ''
+
+export const API_URL = ''
+export const FUND_ID = 2
+export const EVENT_ID = 1
+
+export const WEBSOCKET_URL = ''
