a2f8daf797b00e3fa63e2df687fa39ec70eee230
quentin <quentinrichard97@gmail.com>
2020-01-19 19:07:13 +0100

    remove all wrong code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
 import App from './App';
+import './css/index.scss';
+import { createStore } from "./api/state";
+import {Provider} from 'react-redux';
+import { BrowserRouter as Router } from "react-router-dom";
 
-import {store} from './store'
-import {Provider} from 'react-redux'
+const store = createStore({});
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>
   , document.getElementById('root')
 );
